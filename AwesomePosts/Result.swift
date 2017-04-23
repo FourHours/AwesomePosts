e47fb09dbbc9dd6bdc8dcdfcfb083a70f9fc916b
Q Zhuang <eric@Qs-MacBook-Pro.local>
2017-04-23 20:49:05 +0100

    Stack data structure support

---
+++ AwesomePosts/Result.swift
@@ -0,0 +1,9 @@
+//
+//  Result.swift
+//  AwesomePosts
+//
+//  Created by Q Zhuang on 4/18/17.
+//  Copyright © 2017 Q Zhuang. All rights reserved.
+//
+
+import Foundation
---
+++ AwesomePosts/Stack.swift
@@ -0,0 +1,9 @@
+//
+//  Stack.swift
+//  AwesomePosts
+//
+//  Created by Q Zhuang on 4/23/17.
+//  Copyright © 2017 Q Zhuang. All rights reserved.
+//
+
+import Foundation
